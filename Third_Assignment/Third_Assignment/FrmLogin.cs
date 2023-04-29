42eff78421b01a55b908f903d1eb18f6637683c2
mbourlier-foi <mbourlier23@student.foi.hr>
2023-04-29 17:33:43 +0200

    Creates Login Form.

--- Third_Assignment/Third_Assignment/FrmLogin.cs
+++ Third_Assignment/Third_Assignment/FrmLogin.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Third_Assignment
 {
-    public partial class Form1 : Form
+    public partial class FrmLogin : Form
     {
-        public Form1()
+        public FrmLogin()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
