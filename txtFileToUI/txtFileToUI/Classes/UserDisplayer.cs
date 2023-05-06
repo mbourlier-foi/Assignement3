b3bc16595ba840971b4427f45746e167f6b29efb
mbourlier-foi <mbourlier23@student.foi.hr>
2023-05-06 17:51:24 +0200

    #4 Display data from file

--- txtFileToUI/txtFileToUI/Classes/UserDisplayer.cs
+++ txtFileToUI/txtFileToUI/Classes/UserDisplayer.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace txtFileToUI.Classes
 {
-    internal class UserDisplayer
+    public class UserDisplayer
     {
+        public UserDisplayer()
+        {
+        
+        }
+
+        public void DisplayUserInfo(FrmUserDataDisplay form, UserInfoForm user)
+        {
+            if (user == null)
+            {
+                Console.WriteLine("There is no such user to display");
+            }
+
+            if (form != null)
+            {
+                /* Modify the text of the textboxes */
+                form.Controls["FullNameTextBox"].Text = user.FullName;
+                form.Controls["YearOfBirthTextBox"].Text = user.YearOfBirth.ToString();
+                form.Controls["CityTextBox"].Text = user.CityOfOrigin;
+                form.Controls["FacultyTextBox"].Text = user.Faculty;
+                form.Controls["RoleTextBox"].Text = user.Role;
+                form.Controls["RoleSpecificAttributeTextBox"].Text = user.RoleSpecificAttribute;
+            }
+            else
+            {
+                Console.WriteLine("There is no form to display the user");
+            }
+        }
     }
 }
--- txtFileToUI/txtFileToUI/Classes/UserInfoForm.cs
+++ txtFileToUI/txtFileToUI/Classes/UserInfoForm.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace txtFileToUI.Classes
 {
-    internal class UserInfoForm
+    public class UserInfoForm
     {
+        public string FullName { get; set; }
+        public int YearOfBirth { get; set; }
+        public string CityOfOrigin { get; set; }
+        public string Faculty { get; set; }
+        public string FavoriteCourse { get; set; }
+        public string Role { get; set; }
+        public string RoleSpecificAttribute { get; set; }
+
+        public UserInfoForm()
+        {
+
+        }
     }
 }
